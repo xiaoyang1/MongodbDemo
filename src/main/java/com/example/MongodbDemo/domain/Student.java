3527a6e71d4b8737d9eea1664f03968320ba9f5c
xiaoyang1 <954597783@qq.com>
2018-01-25 16:10:01 +0800

    comlete pedestrianDao

--- src/main/java/com/example/MongodbDemo/domain/Student.java
+++ src/main/java/com/example/MongodbDemo/domain/Student.java
@@ -3,12 +3,13 @@ package com.example.MongodbDemo.domain;
 
 import lombok.Data;
 import org.ietf.jgss.Oid;
+import org.springframework.data.annotation.Id;
 
 import java.io.Serializable;
 
 @Data
 public class Student implements Serializable{
-
+    @Id
     private String _id;
     private String name;
     private int age;
